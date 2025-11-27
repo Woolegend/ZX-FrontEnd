766a5656c1c25784746b26687ab296f88f74ae68
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-11-27 16:14:48 +0900

    feat: add SearchField component

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -2,7 +2,7 @@ import { SearchIcon } from 'lucide-react';
 
 export default function SearchField() {
   return (
-    <div className="focus-within:outline-2 flex gap-4 px-4 py-2 w-fit rounded-full items-center">
+    <div className="focus-within:outline-2 flex px-4 py-2 w-fit rounded-full items-center  outline-brand/80 focus-within:gap-4">
       <label htmlFor="search">
         <SearchIcon size={18} />
       </label>
