0328b8097c6aeaa4e74c8118746224cdac3f0ea3
JaeHyeon, Woo <dnwogus4260@naver.com>
2026-01-05 20:58:20 +0900

    chore: remove sidebar for implement new sidebar

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,16 +1,13 @@
 import { SessionProvider } from 'next-auth/react';
 
 import ReactQueryProvider from '@/components/ReactQueryProvider';
-import SidebarLayout from '@/layouts/SidebarLayout';
 
 export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <SessionProvider>
-      <ReactQueryProvider>
-        <SidebarLayout>{children}</SidebarLayout>
-      </ReactQueryProvider>
+      <ReactQueryProvider>{children}</ReactQueryProvider>
     </SessionProvider>
   );
 }
